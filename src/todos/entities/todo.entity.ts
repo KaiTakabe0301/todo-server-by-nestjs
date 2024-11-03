b3ae167ed410c8db63dc9a914147b9c8d0f26db4
toefuVox <ph.c11.java.eo12@gmail.com>
2024-11-04 06:22:03 +0900

    openapi.yaml の各エンドポイントに、レスポンスの型を追加

--- src/todos/entities/todo.entity.ts
+++ src/todos/entities/todo.entity.ts
@@ -1,1 +1,25 @@
-export class Todo {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Todo } from '@prisma/client';
+
+export class TodoEntity implements Todo {
+  @ApiProperty({
+    example: 'sla5rsk8r0btp7328dxh9k1d',
+    description: 'CUIDで生成されます',
+  })
+  id: string;
+
+  @ApiProperty({ example: 'Buy milk' })
+  title: string;
+
+  @ApiProperty({ default: false })
+  completed: boolean;
+
+  @ApiProperty({ required: false, nullable: true })
+  expiresAt: Date | null;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -11,7 +11,13 @@ import {
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
-import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { TodoEntity } from 'src/todos/entities/todo.entity';
 
 @Controller('todos')
 @ApiTags('todos')
@@ -19,11 +25,16 @@ export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
   @Post()
+  @ApiCreatedResponse({
+    description: 'The record has been successfully created.',
+    type: TodoEntity,
+  })
   create(@Body() createTodoDto: CreateTodoDto) {
     return this.todosService.create(createTodoDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: TodoEntity, isArray: true })
   @ApiQuery({ name: 'completed', required: false, type: Boolean })
   findAll(@Query('completed') completed?: string) {
     if (completed === undefined) {
@@ -34,16 +45,19 @@ export class TodosController {
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: TodoEntity })
   findOne(@Param('id') id: string) {
     return this.todosService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: TodoEntity })
   update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todosService.update(id, updateTodoDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: TodoEntity })
   remove(@Param('id') id: string) {
     return this.todosService.remove(id);
   }
