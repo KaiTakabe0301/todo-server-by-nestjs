efd85896e52d9e817b8829589a9f8afa158f57a3
toefuVox <ph.c11.java.eo12@gmail.com>
2024-11-04 06:28:34 +0900

    openapi.yamlを出力するように修正

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,10 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+
+  // Write OpenAPI spec to openapi.yaml
+  writeFileSync('./openapi.yaml', JSON.stringify(document, null, 2));
+
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
